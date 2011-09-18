b681791fb87515eef7894ef841dbf1f7deb080e8
Bob Corsaro <bob@embed.ly>
2011-09-18 13:45:26 -0400

    fix facebook censor

--- facebook.com.js
+++ facebook.com.js
@@ -1,15 +1,21 @@
 try {
-  $('body').append('<div style="position:fixed;top:10px;left:10px;width:90px;height:30px;background-color:black;font-size:28px;padding:10px"><a id="censor" href="#" style="color:white">Censor</a></div>')
+  $('body').append('<div style="position:fixed;top:30px;left:10px;width:35px;height:10px;background-color:black;font-size:10px;padding:5px;z-index:1000"><a id="censor" href="#" style="color:white">Censor</a></div>')
   $('#censor').click(function() {
     $('.actorName a, .uiAttachmentDetails a, .uiStreamMessage a, .UIImageBlock_Content a').each(function() {
-      console.log($(this).text())
       if ($(this).text().match(/Margaret.Groves.Corsaro/)) {
-        $(this).text("My Mom")
-      } else if (!$(this).text().match(/(status|Wall|others|My.Mom)/)) {
-        $(this).css({color: 'black', 'background-color': 'black'})
+        $(this).text("Mom on Facebook")
+      } else if ($(this).text().match(/Robert.Corsaro/)) {
+        $(this).text("Embarrassed Son")
       }
     })
-    $('img.uiProfilePhoto').attr('src', 'http://doki-pen.org/~doki_pen/black26x26.png')
+    $('.actorName').each(function() {
+      var txt = $(this).text()
+      if (!txt.match(/(status|Wall|others|Margaret.Groves.Corsaro|Robert.Corsaro|Bob.Corsaro|Mom.on.Facebook|Embarrassed.Son)/)) {
+        $(this).text($(this).text().split(' ')[0])
+      }
+    })
+    $('a.actorPic img').attr('src', 'http://doki-pen.org/~doki_pen/blank26x26.gif')
+    $('img.uiProfilePhoto').attr('src', 'http://doki-pen.org/~doki_pen/blank26x26.gif')
   })
 } catch(e) {
   console.log(e)
